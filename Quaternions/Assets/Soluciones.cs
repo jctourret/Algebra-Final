be19cbb8728f329aa7b1027b6426026eecd944bc
Juan Cruz Tourret <jctourret@gmail.com>
2021-12-09 06:58:54 -0300

    Added previous projects

---
+++ Quaternions/Assets/Soluciones.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CustomMath;
+using MathDebbuger;
+using UnityEngine;
+
+public class Soluciones : MonoBehaviour
+{
+    public enum ejercicios
+    {
+        Uno,
+        Dos,
+        Tres
+    }
+    public float angle;
+    public ejercicios ejercicio = ejercicios.Uno;
+
+    public Transform ejer1;
+
+    public List<Vector3> ejer2;
+    public Transform ejer2_1;
+    public Transform ejer2_2;
+    public Transform ejer2_3;
+
+    public List<Vector3> ejer3;
+    public Transform ejer3_1;
+    public Transform ejer3_2;
+    public Transform ejer3_3;
+    public Transform ejer3_4;
+    public Vector3 ejer3RotationPoint = new Vector3(5f,5f,0.0f);
+    public Vector3 ejer3RotationAxis;
+    public float rotationOpen = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ejer3RotationAxis = ejer3RotationPoint.normalized;
+        VectorDebugger.EnableCoordinates();
+        VectorDebugger.AddVector(ejer1.position,Color.red,"Rojo");
+        ejer2.Add(Vector3.zero);
+        ejer2.Add(ejer2_1.forward*10f);
+        ejer2.Add(ejer2_2.up*10f + ejer2_1.forward * 10f);
+        ejer2.Add(ejer2_3.forward * 10f + ejer2_2.up * 10f + ejer2_1.forward * 10f);
+        VectorDebugger.AddVectorsSecuence(ejer2,true,Color.blue,"Azul");
+        ejer3.Add(Vector3.zero);
+        ejer3.Add(ejer3_1.forward * 10f);
+        ejer3.Add(ejer3_2.up * 10f + ejer3_1.forward * 10f);
+        ejer3.Add(ejer3_3.forward * 10f + ejer3_2.up * 10f + ejer3_1.forward * 10f);
+        ejer3.Add(ejer3_4.up * 10f + ejer3_3.forward * 10f + ejer3_2.up * 10f + ejer3_1.forward * 10f);
+        VectorDebugger.AddVectorsSecuence(ejer3, true, Color.yellow, "Amarillo");
+        VectorDebugger.EnableEditorView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(ejercicio){
+            case ejercicios.Uno:
+                ejer1.rotation *= Quats.AngleAxis(angle,new Vec3(transform.up));
+                VectorDebugger.UpdatePosition("Rojo",ejer1.forward*10);
+                break;
+            case ejercicios.Dos:
+                ejer2_1.rotation *= Quats.AngleAxis(angle, new Vec3(transform.up));
+
+                ejer2[1] = ejer2_1.forward * 10f;
+                ejer2[2] = ejer2_2.up * 10f + ejer2_1.forward * 10f;
+                ejer2[3] = ejer2_3.forward * 10f + ejer2_2.up * 10f + ejer2_1.forward * 10f;
+
+                VectorDebugger.UpdatePositionsSecuence("Azul",ejer2);
+                break;
+            case ejercicios.Tres:
+                
+                ejer3_1.rotation *= Quats.AngleAxis(angle,new Vec3(ejer3RotationAxis));
+                ejer3_3.rotation *= Quats.AngleAxis(angle, new Vec3(ejer3RotationAxis));
+
+                ejer3[1] = ejer3_1.forward * rotationOpen + ejer3_1.position;
+                ejer3[2] = ejer3_2.position;
+                ejer3[3] = ejer3_3.forward * rotationOpen + ejer3_3.position;
+                ejer3[4] = ejer3_4.position;
+
+                VectorDebugger.UpdatePositionsSecuence("Amarillo", ejer3);
+                break;
+        }
+    }
+}
---
+++ TP-Algebra/Assets/Scripts/MathDebbuger/Tests/Tester.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MathDebbuger;
+using CustomMath;
+using UnityEngine.XR.WSA.Input;
+
+public class Tester : MonoBehaviour
+{
+    public Vector3 ejerA;
+    public Vector3 ejerB;
+    Vec3 result;
+    float timer = 0;
+
+    public enum Ejercicios
+    {
+        uno,
+        dos,
+        tres,
+        cuatro,
+        cinco,
+        seis,
+        siete,
+        ocho,
+        nueve,
+        diez,
+    }
+
+    public Ejercicios ejer = 0;
+
+    void Start()
+    {
+        VectorDebugger.EnableCoordinates();
+        List<Vector3> vectors = new List<Vector3>();
+        VectorDebugger.AddVector(ejerA, Color.black, "elNegro");
+        VectorDebugger.AddVector(ejerB, Color.yellow, "elAmarillo");
+        vectors.Add(new Vec3(10.0f, 0.0f, 0.0f));
+        vectors.Add(new Vec3(10.0f, 10.0f, 0.0f));
+        vectors.Add(new Vec3(20.0f, 10.0f, 0.0f));
+        vectors.Add(new Vec3(20.0f, 20.0f, 0.0f));
+        VectorDebugger.AddVectorsSecuence(vectors, false, Color.red, "secuencia");
+        VectorDebugger.AddVector(result, Color.white, "elBlanco");
+        VectorDebugger.EnableEditorView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vec3 A = new Vec3(ejerA);
+        Vec3 B = new Vec3(ejerB);
+        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            VectorDebugger.TurnOffVector("elAzul");
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            VectorDebugger.TurnOnVector("elAzul");
+        }
+        switch (ejer) {
+            case Ejercicios.uno:
+            result = A + B;
+            break;
+            case Ejercicios.dos:
+            result = A - B;
+            break;
+            case Ejercicios.tres:
+            result.Scale(A,B);
+            break;
+            case Ejercicios.cuatro:
+            result = Vec3.Cross(A,B);
+            break;
+            case Ejercicios.cinco:
+            timer += Time.deltaTime;
+            result = Vec3.Lerp(A,B,timer);
+            if (timer > 1) { timer = 0; };
+            break;
+            case Ejercicios.seis:
+            result = Vec3.Max(A,B);
+            break;
+            case Ejercicios.siete:
+            result = Vec3.Project(A,B);
+            break;
+            case Ejercicios.ocho:
+               
+            break;
+            case Ejercicios.nueve:
+                result = Vec3.Reflect(A, B);
+                break;
+            case Ejercicios.diez:
+            float t = 0;
+            t += Time.deltaTime;
+            result = Vec3.LerpUnclamped(A,B,t);
+            break;
+        }
+        VectorDebugger.UpdatePosition("elNegro", A);
+        VectorDebugger.UpdatePosition("elAmarillo", B);
+        VectorDebugger.UpdatePosition("elBlanco", result);
+    }
+}
